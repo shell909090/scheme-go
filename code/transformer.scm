40402af0a8c7ea1d7dc2507014aa34ff22f72c93
Shell.Xu <shell909090@gmail.com>
2015-07-13 11:35:04 +0800

    * begin to use scheme impl transformer.

---
+++ code/transformer.scm
@@ -0,0 +1,12 @@
+(define (match-list plist olist literals result))
+(define (match plist olist literals result))
+
+(define (render-list result template))
+(define (render result template)
+
+(define (parse-define-syntax code))
+(define (parse-syntax-rules code))
+(define (syntax-transforme code))
+
+(define (parse-transformer code))
+(define (transform-code code))
---
+++ tsfm/main.go
@@ -0,0 +1,5 @@
+package tsfm
+
+func main() {
+
+}
